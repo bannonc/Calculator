2da937c27e4e71529f2e916f36ffdf069fe0c4e5
curis <chrisbannondub@gmail.com>
2013-04-11 11:26:07 +0100

    Initial Commit

---
+++ Calculator/src/ie/gcd/maths/Calc.java
@@ -0,0 +1,22 @@
+package ie.gcd.maths;
+
+public class Calc {
+	
+	public Calc(){
+		
+		
+	}
+
+	public int add(int x, int y){
+		return x-y;
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Calculator/src/ie/gcd/test/CalcTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package ie.gcd.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Chris Bannon
+ *
+ */
+public class CalcTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link ie.gcd.maths.Calc#add(int, int)}.
+	 */
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+}
